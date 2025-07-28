8784c44485a96e548825bbc723d5c4e2df968eb8
hibby <hibaqku7@gmail.com>
2025-07-28 23:12:39 +0300

    Minor fixes and updates to Login, Map, and Report pages

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';
 import logo from '../assets/alerticon.png';
-import { jwtDecode } from 'jwt-decode'; // Correct named import
+import { jwtDecode } from 'jwt-decode'; 
 
 const Login = () => {
   const navigate = useNavigate();
